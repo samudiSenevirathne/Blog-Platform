6c07756cb84b24fdd415d9c2d7d4493950c16acf
samudiSenevirathne <stsamudi@gmail.com>
2024-01-07 20:22:47 +0530

    Content Updated

--- src/view/common/Footer/Footer.tsx
+++ src/view/common/Footer/Footer.tsx
@@ -4,7 +4,8 @@ export class Footer extends Component {
     render() {
         return (
             <div>
-                <h1 className="text-black text-1xl text-center">Developed By Samudi|2024 All Right Reserved</h1>
+                <h1 className="text-black text-1xl text-center font-bold">Developed By
+                    <span className="text-gray-500"> Samudi</span> | &copy; 2024 All Right Reserved</h1>
             </div>
         );
     }
--- src/view/common/Navbar/Navbar.tsx
+++ src/view/common/Navbar/Navbar.tsx
@@ -4,7 +4,27 @@ export class Navbar extends Component {
     render() {
         return (
             <div className="flex justify-between">
-                  <h1 className="text-black text-2xl">MyBlog</h1>
+                  <h1 className="text-black text-2xl font-bold">MyBlog</h1>
+
+                <ul className="flex list-none mt-1">
+                    <li className="inline-block mr-5 text-1xl text-black cursor-pointer">Home</li>
+
+                    {/*for future below li*/}
+                    {/*<li className="inline-block mr-5 text-1xl text-black cursor-pointer ">Create New Post</li>*/}
+                    {/*<li className="inline-block  text-1xl text-black ">(Samudi)</li>*/}
+
+                    {/*for future below button*/}
+                    {/*<button*/}
+                    {/*    className="text-1xl text-white bg-black pl-4 pr-4 mr-5 hover:bg-black/70 font-bold rounded-b-lg rounded-t-lg text-center">SignIn*/}
+                    {/*</button>*/}
+
+                    <button
+                        className="text-1xl text-white bg-black pl-4 pr-4 mr-5 hover:bg-black/70 font-bold rounded-b-lg rounded-t-lg text-center">SignIn
+                    </button>
+
+                </ul>
+
+
             </div>
         );
     }
