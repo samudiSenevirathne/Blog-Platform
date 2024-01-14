51811dd49578640ebfdac7a9f894642069a96166
samudiSenevirathne <stsamudi@gmail.com>
2024-01-15 00:31:53 +0530

    Content Updated

--- src/view/common/Main/Main.tsx
+++ src/view/common/Main/Main.tsx
@@ -2,14 +2,16 @@ import {Component} from "react";
 import {Route, Routes} from "react-router-dom";
 import {Home} from "../../pages/Home/Home";
 import {Login} from "../../pages/Login/Login";
+import {CreateAccount} from "../../pages/CreateAccount/CreateAccount";
 
 export class Main extends Component {
     render() {
         return (
             <div className="mx-auto">
             <Routes>
-            <Route path="/" Component={Home}></Route>
+             <Route path="/" Component={Home}></Route>
             <Route path="/Login" Component={Login}></Route>
+            <Route path="/CreateAccount" Component={CreateAccount}></Route>
             </Routes>
             </div>
         );
--- src/view/pages/CreateAccount/CreateAccount.tsx
+++ src/view/pages/CreateAccount/CreateAccount.tsx
@@ -1,17 +1,33 @@
 import {Component} from "react";
 import {Link} from "react-router-dom";
 
-export class Login extends Component {
+export class CreateAccount extends Component {
     render() {
         return (
             <div className="flex min-h-full flex-col justify-center px-6 py-8 border-black border-2 rounded-md mt-5 ml-5 mr-5 mb-0 ">
                 <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                        <h2 className="mt-10 text-center text-2xl font-bold leading-9
-                         text-black">L o g i n</h2>
+                    <h2 className="mt-10 text-center text-2xl font-bold leading-9
+                         text-black">Create New Account</h2>
                 </div>
 
                 <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
                     <form className="space-y-6">
+                        <div>
+                            <label className="block text-sm font-bold leading-6 text-black">Email</label>
+                            <div className="mt-2">
+                                <input className="block w-full rounded-md border-2 border-black py-1.5 text-black shadow-lg shadow-black
+                                placeholder:text-gray-40 sm:text-sm sm:leading-6" type="email" required/>
+                            </div>
+                        </div>
+
+                        <div>
+                            <label className="block text-sm font-bold leading-6 text-black">Name</label>
+                            <div className="mt-2">
+                                <input className="block w-full rounded-md border-2 border-black py-1.5 text-black shadow-lg shadow-black
+                                placeholder:text-gray-40 sm:text-sm sm:leading-6" type="text" required/>
+                            </div>
+                        </div>
+
                         <div>
                             <label className="block text-sm font-bold leading-6 text-black">Username</label>
                             <div className="mt-2">
@@ -30,18 +46,17 @@ export class Login extends Component {
 
                         <div>
                             <button className="flex w-32 mt-10 mx-auto justify-center rounded-md bg-black px-3 py-1.5
-                             text-sm font-semibold leading-6 text-white shadow-sm hover:bg-black/90" type="button">Login
+                             text-sm font-semibold leading-6 text-white shadow-sm hover:bg-black/90" type="button">Submit
                             </button>
                         </div>
                     </form>
 
                     <h1 className="mt-5 font-bold text-center text-sm text-black">Or</h1>
                     <h1 className=" mt-3 font-bold text-center text-sm text-black hover:cursor-pointer">
-                        <Link to="/CreateAccount">create a new account</Link>
+                        <Link to="/Login">Have an account? Login.</Link>
                     </h1>
                 </div>
-                </div>
-
+            </div>
         );
     }
 }
